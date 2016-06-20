22c3f1b32f80e5ca0a728a35f5589cbadbf8f619
luyishisi <543429245@qq.com>
2016-06-20 17:30:49 +0800

    add  w3c 注释

--- w3school/w3school/pipelines.py
+++ w3school/w3school/pipelines.py
@@ -10,10 +10,14 @@ import codecs
   
 class W3SchoolPipeline(object):  
     def __init__(self):  
+        #初始化打开json记录文件数据
+        #定义编码为utf-8
         self.file = codecs.open('w3school_data_utf8.json', 'wb', encoding='utf-8')  
   
     def process_item(self, item, spider):  
         line = json.dumps(dict(item)) + '\n'  
         # print line  
+        
         self.file.write(line.decode("unicode_escape"))  
         return item  
+        #返回类型必须要是item类型
