fe2a9e1c26171cbf7712f87dc6f5197885f4aeb5
luyishisi <543429245@qq.com>
2016-07-12 10:39:30 +0800

    修正一些meizitu的bug。还有下载问题

--- scrapy_code/meizitu/meizitu/spiders/__init__.py
+++ scrapy_code/meizitu/meizitu/spiders/__init__.py
@@ -4,7 +4,7 @@
 # Please refer to the documentation for information on how to create and manage
 # your spiders.
 import scrapy
-from scrapy import log
-#开启注释
-log.msg("This is a warning", level=log.WARNING)
-scrapy.log.start()
+#from scrapy import log
+#开启注释,在部分机器上会报错，因为环境没有搭建好
+#log.msg("This is a warning", level=log.WARNING)
+#scrapy.log.start()
